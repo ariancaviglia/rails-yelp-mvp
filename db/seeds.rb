23fdcb55916f9f7cae104689256c4d00eca9d0f0
arian caviglia <ariancaviglia7@gmail.com>
2020-07-22 17:31:32 -0300

    Ejercicio 2 - Clase 28

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.delete_all
+5.times do
+  Restaurant.create(
+  name: Faker::Restaurant.name,
+  address: Faker::Address.street_address,
+  phone_number: Faker::PhoneNumber,
+  category: "chinese" 
+ )
+end
+puts "seeds finished"
